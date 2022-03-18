bf6d2f384ae0c95f827b562157ceaccf6686d3cf
4war <fedro_777@mail.ru>
2022-03-18 10:55:33 +0400

    Added MySQL

---
+++ Api.Rtt/Models/Seeds/DataSeed.cs
@@ -0,0 +1,17 @@
+﻿namespace Api.Rtt.Models.Seeds
+{
+    public class DataSeed
+    {
+        private ApiContext _context;
+        
+        public DataSeed(ApiContext apiContext)
+        {
+            _context = apiContext;
+        }
+
+        public void SeedData()
+        {
+            _context.Database.EnsureCreated();
+        }
+    }
+}
